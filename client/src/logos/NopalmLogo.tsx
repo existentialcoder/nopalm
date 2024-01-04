6f5ee5c39eeb8370391c0007cfae25bbb318145c
existentialcoder <shravanayyappa@gmail.com>
2024-01-05 03:24:59 +0530

    Frontend: React + TS + Vite Skeleton setup

---
+++ client/src/logos/NopalmLogo.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import NopalmLogoImg from '../assets/nopalm.svg';
+
+import './NopalmLogo.scss';
+
+interface NopalmLogoProps {
+    includeTitle?: Boolean,
+};
+
+const NopalmLogo: React.FC<NopalmLogoProps> = (props: NopalmLogoProps) => {
+    return (
+        <div className="icon-container">
+            <img src={NopalmLogoImg} width={"25%"} height={"100%"} alt="Nopalm" />
+            {
+                props.includeTitle ? <div className="nopalm-title">
+                    NOPALM
+                </div> : ''    
+            }
+        </div>
+    )
+};
+
+export default NopalmLogo;
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import ProjectDetails from './ProjectDetails';
+import Packages from './Packages';
+import Settings from './Settings';
+
+export {
+    ProjectDetails, Packages, Settings
+};
